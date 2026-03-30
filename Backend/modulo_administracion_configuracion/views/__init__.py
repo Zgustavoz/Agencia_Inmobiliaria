db46e632c8f672d413a8195efe37c6b030a605b9
NowIose <dainerdual@gmail.com>
2026-03-29 23:21:36 -0400

    feat(users): implementar bitácora inicial y registro de login/registro
    Se agregó el modelo ActividadSistema, el servicio BitacoraService y la integración en los endpoints de autenticación. Requiere ejecutar migrate.

--- Backend/modulo_administracion_configuracion/views/__init__.py
+++ Backend/modulo_administracion_configuracion/views/__init__.py
@@ -1,1 +1,2 @@
-from .moneda import MonedaListCreateView, MonedaDetailView
+#from .moneda import MonedaListCreateView, MonedaDetailView 
+#comente esto porque me tira que que aun no exiten esos dos procedimientos
