aac912173d8833790890c26a54c81369a23d4b5d
Zgustavoz <gustavoibarra323@gmail.com>
2026-03-30 23:27:35 -0400

    añadiendo mensajes y redi...

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -8,6 +8,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import { Toaster } from "react-hot-toast"
 // TODO: AGREGAR LA API DE GOOGLE O MAPBOX, INVESTIGAR CUAL ES MEJOR PARA EL AUTOCOMPLETE DE DIRECCIONES Y MAPAS EN GENERAL
 // import { APIProvider } from "@vis.gl/react-google-maps";
 
@@ -19,6 +20,7 @@ createRoot(document.getElementById('root')).render(
       <QueryClientProvider client={queryClient}>
         {/* <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}> */}
         <InmobiliarApp />
+        <Toaster position="top-right" />
         {/* </APIProvider> */}
       </QueryClientProvider>
     </BrowserRouter>
