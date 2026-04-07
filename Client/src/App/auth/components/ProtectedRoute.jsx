b5233ad18fa529400c35245409f5e001e0ea9500
Zgustavoz <gustavoibarra323@gmail.com>
2026-04-06 21:42:06 -0400

    subiendo a produccion

---
+++ Client/src/App/auth/components/ProtectedRoute.jsx
@@ -0,0 +1,24 @@
+import { Navigate } from "react-router"
+import { useAuth } from "../context/AuthContext"
+import { Loading } from "../../../shared/ui/components/Loading"
+import { UnauthorizedPage } from "../../../shared/ui/components/UnauthorizedPage"
+
+export const ProtectedRoute = ({
+  children,
+  requiredPermission = null,
+  requiredRoles      = [],
+}) => {
+  const { user, loading, tienePermiso, tieneAcceso } = useAuth()
+
+  if (loading) return <Loading />
+  // if (!user)   return <Navigate to="/auth" replace />
+  if (user.es_admin) return children
+
+  if (requiredPermission && !tienePermiso(requiredPermission))
+    return <UnauthorizedPage />
+
+  if (requiredRoles.length > 0 && !tieneAcceso(requiredRoles))
+    return <UnauthorizedPage />
+
+  return children
+}
