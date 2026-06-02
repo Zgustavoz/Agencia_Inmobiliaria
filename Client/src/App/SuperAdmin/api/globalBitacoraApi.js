74cf33d8eabde691ed1d16bc43ef43f5c3638f09
Zgustavoz <gustavoibarra323@gmail.com>
2026-06-02 07:37:04 -0400

    nuevos c

--- Client/src/App/SuperAdmin/api/globalBitacoraApi.js
+++ Client/src/App/SuperAdmin/api/globalBitacoraApi.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000"
+const API_URL = import.meta.env.VITE_APP_BASE_URL || "http://localhost:8000"
 
 export const listarBitacoraGlobal = async () => {
   const response = await axios.get(`${API_URL}/gestion_usuarios/global-bitacora/`, {
