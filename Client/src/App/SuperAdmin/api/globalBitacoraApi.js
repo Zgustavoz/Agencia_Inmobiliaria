af027a66c56978e6544e79698df34c465b47eca1
NowIose <dainerdual@gmail.com>
2026-06-17 22:06:27 -0400

    arreglado superadmin

--- Client/src/App/SuperAdmin/api/globalBitacoraApi.js
+++ Client/src/App/SuperAdmin/api/globalBitacoraApi.js
@@ -1,19 +1,14 @@
-import axios from "axios"
-
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000"
+import { intanciaAxios } from "../../../config/axios"
 
 export const listarBitacoraGlobal = async () => {
-  const response = await axios.get(`${API_URL}/gestion_usuarios/global-bitacora/`, {
-    withCredentials: true,
-  })
+  const response = await intanciaAxios.get("/gestion_usuarios/global-bitacora/")
   return response.data
 }
 
 export const verificarClaveBitacoraGlobal = async (password) => {
-  const response = await axios.post(
-    `${API_URL}/gestion_usuarios/global-bitacora/verificar-clave/`,
-    { password },
-    { withCredentials: true }
+  const response = await intanciaAxios.post(
+    "/gestion_usuarios/global-bitacora/verificar-clave/",
+    { password }
   )
   return response.data
 }
